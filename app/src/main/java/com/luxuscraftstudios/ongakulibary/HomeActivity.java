436e8b684d7eb08078eea82d32a03cfde7aeef7a
justinweichTV <64624657+justinweichTV@users.noreply.github.com>
2021-09-09 05:06:21 +0200

    surcecode

---
+++ app/src/main/java/com/luxuscraftstudios/ongakulibary/HomeActivity.java
@@ -0,0 +1,261 @@
+package com.luxuscraftstudios.ongakulibary;
+
+import android.app.Activity;
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+import android.content.*;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.webkit.*;
+import android.animation.*;
+import android.view.animation.*;
+import java.util.*;
+import java.util.regex.*;
+import java.text.*;
+import org.json.*;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import android.widget.Button;
+import android.os.Bundle;
+import java.io.InputStream;
+import android.content.Intent;
+import android.net.Uri;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.view.View;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.DialogFragment;
+
+
+public class HomeActivity extends  Activity { 
+	
+	
+	private LinearLayout linear1;
+	private LinearLayout linear2;
+	private LinearLayout linear5;
+	private TextView textview1;
+	private WebView webview1;
+	private LinearLayout linear3;
+	private LinearLayout linear4;
+	private Button button1;
+	private Button button2;
+	private Button button3;
+	
+	private Intent Szene = new Intent();
+	private AlertDialog.Builder dialog;
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.home);
+		initialize(_savedInstanceState);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		
+		linear1 = (LinearLayout) findViewById(R.id.linear1);
+		linear2 = (LinearLayout) findViewById(R.id.linear2);
+		linear5 = (LinearLayout) findViewById(R.id.linear5);
+		textview1 = (TextView) findViewById(R.id.textview1);
+		webview1 = (WebView) findViewById(R.id.webview1);
+		webview1.getSettings().setJavaScriptEnabled(true);
+		webview1.getSettings().setSupportZoom(true);
+		linear3 = (LinearLayout) findViewById(R.id.linear3);
+		linear4 = (LinearLayout) findViewById(R.id.linear4);
+		button1 = (Button) findViewById(R.id.button1);
+		button2 = (Button) findViewById(R.id.button2);
+		button3 = (Button) findViewById(R.id.button3);
+		dialog = new AlertDialog.Builder(this);
+		
+		//webviewOnProgressChanged
+		webview1.setWebChromeClient(new WebChromeClient() {
+				@Override public void onProgressChanged(WebView view, int _newProgress) {
+					
+				}
+		});
+		
+		//OnDownloadStarted
+		webview1.setDownloadListener(new DownloadListener() {
+			public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+				DownloadManager.Request webview1a = new DownloadManager.Request(Uri.parse(url));
+				String webview1b = CookieManager.getInstance().getCookie(url);
+				webview1a.addRequestHeader("cookie", webview1b);
+				webview1a.addRequestHeader("User-Agent", userAgent);
+				webview1a.setDescription("Downloading file...");
+				webview1a.setTitle(URLUtil.guessFileName(url, contentDisposition, mimetype));
+				webview1a.allowScanningByMediaScanner(); webview1a.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED); webview1a.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(url, contentDisposition, mimetype));
+				
+				DownloadManager webview1c = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+				webview1c.enqueue(webview1a);
+				showMessage("Downloading File....");
+				BroadcastReceiver onComplete = new BroadcastReceiver() {
+					public void onReceive(Context ctxt, Intent intent) {
+						showMessage("Download Complete!");
+						unregisterReceiver(this);
+						
+					}};
+				registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+			}
+		});
+		
+		//OnTouch
+		webview1.setOnTouchListener(new View.OnTouchListener(){
+				@Override
+				public boolean onTouch(View v, MotionEvent event){
+						int ev = event.getAction();
+						switch (ev) {
+								case MotionEvent.ACTION_DOWN:
+								
+								 
+								
+								break;
+								case MotionEvent.ACTION_UP:
+								
+								 
+								
+								break;
+						} return true;
+				}
+		});
+		
+		webview1.setWebViewClient(new WebViewClient() {
+			@Override
+			public void onPageStarted(WebView _param1, String _param2, Bitmap _param3) {
+				final String _url = _param2;
+				
+				super.onPageStarted(_param1, _param2, _param3);
+			}
+			
+			@Override
+			public void onPageFinished(WebView _param1, String _param2) {
+				final String _url = _param2;
+				
+				super.onPageFinished(_param1, _param2);
+			}
+		});
+		
+		button1.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				SketchwareUtil.showMessage(getApplicationContext(), "あなたはすでにここにいます!このページを再読み込みします");
+				Szene.setClass(getApplicationContext(), HomeActivity.class);
+				startActivity(Szene);
+			}
+		});
+		
+		button2.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				dialog.setTitle("あなたはライトのオン/オンをするのが好きでしたか?");
+				dialog.setMessage("あなたはライトのオン/オンをするのが好きでしたか");
+				dialog.setPositiveButton("はい (明かりを消す)", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface _dialog, int _which) {
+						linear1.setBackgroundColor(0xFF424242);
+					}
+				});
+				dialog.setNegativeButton("はい (ライトを点灯する)", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface _dialog, int _which) {
+						linear1.setBackgroundColor(0xFFFFFFFF);
+					}
+				});
+				dialog.setNeutralButton("いいえ", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface _dialog, int _which) {
+						
+					}
+				});
+				dialog.create().show();
+			}
+		});
+		
+		button3.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				Szene.setClass(getApplicationContext(), WebActivity.class);
+				startActivity(Szene);
+			}
+		});
+	}
+	
+	private void initializeLogic() {
+		webview1.loadUrl("http://crazycrew.ddns.net/ongaku-libary/music/");
+	}
+	
+	@Override
+	protected void onActivityResult(int _requestCode, int _resultCode, Intent _data) {
+		
+		super.onActivityResult(_requestCode, _resultCode, _data);
+		
+		switch (_requestCode) {
+			
+			default:
+			break;
+		}
+	}
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input){
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels(){
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels(){
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+	
+}
---
+++ app/src/main/java/com/luxuscraftstudios/ongakulibary/WebActivity.java
@@ -0,0 +1,492 @@
+package com.luxuscraftstudios.ongakulibary;
+
+import android.app.Activity;
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+import android.content.*;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.webkit.*;
+import android.animation.*;
+import android.view.animation.*;
+import java.util.*;
+import java.util.regex.*;
+import java.text.*;
+import org.json.*;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.Button;
+import android.widget.TextView;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import java.io.InputStream;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.DialogFragment;
+
+
+public class WebActivity extends  Activity { 
+	
+	
+	private LinearLayout linear1;
+	private LinearLayout linear2;
+	private ScrollView vscroll1;
+	private LinearLayout linear3;
+	private LinearLayout linear4;
+	private Button button1;
+	private Button button2;
+	private Button button3;
+	private LinearLayout linear5;
+	private TextView textview1;
+	private LinearLayout linear6;
+	private TextView textview2;
+	private WebView webview3;
+	private WebView webview4;
+	private WebView webview2;
+	private WebView webview1;
+	
+	private AlertDialog.Builder dialog;
+	private Intent Szene = new Intent();
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.web);
+		initialize(_savedInstanceState);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		
+		linear1 = (LinearLayout) findViewById(R.id.linear1);
+		linear2 = (LinearLayout) findViewById(R.id.linear2);
+		vscroll1 = (ScrollView) findViewById(R.id.vscroll1);
+		linear3 = (LinearLayout) findViewById(R.id.linear3);
+		linear4 = (LinearLayout) findViewById(R.id.linear4);
+		button1 = (Button) findViewById(R.id.button1);
+		button2 = (Button) findViewById(R.id.button2);
+		button3 = (Button) findViewById(R.id.button3);
+		linear5 = (LinearLayout) findViewById(R.id.linear5);
+		textview1 = (TextView) findViewById(R.id.textview1);
+		linear6 = (LinearLayout) findViewById(R.id.linear6);
+		textview2 = (TextView) findViewById(R.id.textview2);
+		webview3 = (WebView) findViewById(R.id.webview3);
+		webview3.getSettings().setJavaScriptEnabled(true);
+		webview3.getSettings().setSupportZoom(true);
+		webview4 = (WebView) findViewById(R.id.webview4);
+		webview4.getSettings().setJavaScriptEnabled(true);
+		webview4.getSettings().setSupportZoom(true);
+		webview2 = (WebView) findViewById(R.id.webview2);
+		webview2.getSettings().setJavaScriptEnabled(true);
+		webview2.getSettings().setSupportZoom(true);
+		webview1 = (WebView) findViewById(R.id.webview1);
+		webview1.getSettings().setJavaScriptEnabled(true);
+		webview1.getSettings().setSupportZoom(true);
+		dialog = new AlertDialog.Builder(this);
+		
+		button1.setOnLongClickListener(new View.OnLongClickListener() {
+			 @Override
+				public boolean onLongClick(View _view) {
+				
+				return true;
+				}
+			 });
+		
+		button1.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				Szene.setClass(getApplicationContext(), HomeActivity.class);
+				startActivity(Szene);
+			}
+		});
+		
+		button2.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				dialog.setTitle("あなたはライトのオン/オンをするのが好きでしたか?");
+				dialog.setMessage("あなたはライトのオン/オンをするのが好きでしたか");
+				dialog.setPositiveButton("はい (明かりを消す)", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface _dialog, int _which) {
+						linear1.setBackgroundColor(0xFF424242);
+					}
+				});
+				dialog.setNegativeButton("はい (ライトを点灯する)", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface _dialog, int _which) {
+						linear1.setBackgroundColor(0xFFFFFFFF);
+					}
+				});
+				dialog.setNeutralButton("いいえ", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface _dialog, int _which) {
+						
+					}
+				});
+				dialog.create().show();
+			}
+		});
+		
+		button3.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				SketchwareUtil.showMessage(getApplicationContext(), "あなたはすでにここにいます!\nこのページを再読み込みします!");
+				Szene.setClass(getApplicationContext(), WebActivity.class);
+				startActivity(Szene);
+			}
+		});
+		
+		//webviewOnProgressChanged
+		webview3.setWebChromeClient(new WebChromeClient() {
+				@Override public void onProgressChanged(WebView view, int _newProgress) {
+					
+				}
+		});
+		
+		//OnDownloadStarted
+		webview3.setDownloadListener(new DownloadListener() {
+			public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+				DownloadManager.Request webview3a = new DownloadManager.Request(Uri.parse(url));
+				String webview3b = CookieManager.getInstance().getCookie(url);
+				webview3a.addRequestHeader("cookie", webview3b);
+				webview3a.addRequestHeader("User-Agent", userAgent);
+				webview3a.setDescription("Downloading file...");
+				webview3a.setTitle(URLUtil.guessFileName(url, contentDisposition, mimetype));
+				webview3a.allowScanningByMediaScanner(); webview3a.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED); webview3a.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(url, contentDisposition, mimetype));
+				
+				DownloadManager webview3c = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+				webview3c.enqueue(webview3a);
+				showMessage("Downloading File....");
+				BroadcastReceiver onComplete = new BroadcastReceiver() {
+					public void onReceive(Context ctxt, Intent intent) {
+						showMessage("Download Complete!");
+						unregisterReceiver(this);
+						
+					}};
+				registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+			}
+		});
+		
+		//OnTouch
+		webview3.setOnTouchListener(new View.OnTouchListener(){
+				@Override
+				public boolean onTouch(View v, MotionEvent event){
+						int ev = event.getAction();
+						switch (ev) {
+								case MotionEvent.ACTION_DOWN:
+								
+								 
+								
+								break;
+								case MotionEvent.ACTION_UP:
+								
+								 
+								
+								break;
+						} return true;
+				}
+		});
+		
+		webview3.setWebViewClient(new WebViewClient() {
+			@Override
+			public void onPageStarted(WebView _param1, String _param2, Bitmap _param3) {
+				final String _url = _param2;
+				
+				super.onPageStarted(_param1, _param2, _param3);
+			}
+			
+			@Override
+			public void onPageFinished(WebView _param1, String _param2) {
+				final String _url = _param2;
+				
+				super.onPageFinished(_param1, _param2);
+			}
+		});
+		
+		//webviewOnProgressChanged
+		webview4.setWebChromeClient(new WebChromeClient() {
+				@Override public void onProgressChanged(WebView view, int _newProgress) {
+					
+				}
+		});
+		
+		//OnDownloadStarted
+		webview4.setDownloadListener(new DownloadListener() {
+			public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+				DownloadManager.Request webview4a = new DownloadManager.Request(Uri.parse(url));
+				String webview4b = CookieManager.getInstance().getCookie(url);
+				webview4a.addRequestHeader("cookie", webview4b);
+				webview4a.addRequestHeader("User-Agent", userAgent);
+				webview4a.setDescription("Downloading file...");
+				webview4a.setTitle(URLUtil.guessFileName(url, contentDisposition, mimetype));
+				webview4a.allowScanningByMediaScanner(); webview4a.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED); webview4a.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(url, contentDisposition, mimetype));
+				
+				DownloadManager webview4c = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+				webview4c.enqueue(webview4a);
+				showMessage("Downloading File....");
+				BroadcastReceiver onComplete = new BroadcastReceiver() {
+					public void onReceive(Context ctxt, Intent intent) {
+						showMessage("Download Complete!");
+						unregisterReceiver(this);
+						
+					}};
+				registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+			}
+		});
+		
+		//OnTouch
+		webview4.setOnTouchListener(new View.OnTouchListener(){
+				@Override
+				public boolean onTouch(View v, MotionEvent event){
+						int ev = event.getAction();
+						switch (ev) {
+								case MotionEvent.ACTION_DOWN:
+								
+								 
+								
+								break;
+								case MotionEvent.ACTION_UP:
+								
+								 
+								
+								break;
+						} return true;
+				}
+		});
+		
+		webview4.setWebViewClient(new WebViewClient() {
+			@Override
+			public void onPageStarted(WebView _param1, String _param2, Bitmap _param3) {
+				final String _url = _param2;
+				
+				super.onPageStarted(_param1, _param2, _param3);
+			}
+			
+			@Override
+			public void onPageFinished(WebView _param1, String _param2) {
+				final String _url = _param2;
+				
+				super.onPageFinished(_param1, _param2);
+			}
+		});
+		
+		//webviewOnProgressChanged
+		webview2.setWebChromeClient(new WebChromeClient() {
+				@Override public void onProgressChanged(WebView view, int _newProgress) {
+					
+				}
+		});
+		
+		//OnDownloadStarted
+		webview2.setDownloadListener(new DownloadListener() {
+			public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+				DownloadManager.Request webview2a = new DownloadManager.Request(Uri.parse(url));
+				String webview2b = CookieManager.getInstance().getCookie(url);
+				webview2a.addRequestHeader("cookie", webview2b);
+				webview2a.addRequestHeader("User-Agent", userAgent);
+				webview2a.setDescription("Downloading file...");
+				webview2a.setTitle(URLUtil.guessFileName(url, contentDisposition, mimetype));
+				webview2a.allowScanningByMediaScanner(); webview2a.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED); webview2a.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(url, contentDisposition, mimetype));
+				
+				DownloadManager webview2c = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+				webview2c.enqueue(webview2a);
+				showMessage("Downloading File....");
+				BroadcastReceiver onComplete = new BroadcastReceiver() {
+					public void onReceive(Context ctxt, Intent intent) {
+						showMessage("Download Complete!");
+						unregisterReceiver(this);
+						
+					}};
+				registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+			}
+		});
+		
+		//OnTouch
+		webview2.setOnTouchListener(new View.OnTouchListener(){
+				@Override
+				public boolean onTouch(View v, MotionEvent event){
+						int ev = event.getAction();
+						switch (ev) {
+								case MotionEvent.ACTION_DOWN:
+								
+								 
+								
+								break;
+								case MotionEvent.ACTION_UP:
+								
+								 
+								
+								break;
+						} return true;
+				}
+		});
+		
+		webview2.setWebViewClient(new WebViewClient() {
+			@Override
+			public void onPageStarted(WebView _param1, String _param2, Bitmap _param3) {
+				final String _url = _param2;
+				
+				super.onPageStarted(_param1, _param2, _param3);
+			}
+			
+			@Override
+			public void onPageFinished(WebView _param1, String _param2) {
+				final String _url = _param2;
+				
+				super.onPageFinished(_param1, _param2);
+			}
+		});
+		
+		//webviewOnProgressChanged
+		webview1.setWebChromeClient(new WebChromeClient() {
+				@Override public void onProgressChanged(WebView view, int _newProgress) {
+					
+				}
+		});
+		
+		//OnDownloadStarted
+		webview1.setDownloadListener(new DownloadListener() {
+			public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+				DownloadManager.Request webview1a = new DownloadManager.Request(Uri.parse(url));
+				String webview1b = CookieManager.getInstance().getCookie(url);
+				webview1a.addRequestHeader("cookie", webview1b);
+				webview1a.addRequestHeader("User-Agent", userAgent);
+				webview1a.setDescription("Downloading file...");
+				webview1a.setTitle(URLUtil.guessFileName(url, contentDisposition, mimetype));
+				webview1a.allowScanningByMediaScanner(); webview1a.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED); webview1a.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(url, contentDisposition, mimetype));
+				
+				DownloadManager webview1c = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+				webview1c.enqueue(webview1a);
+				showMessage("Downloading File....");
+				BroadcastReceiver onComplete = new BroadcastReceiver() {
+					public void onReceive(Context ctxt, Intent intent) {
+						showMessage("Download Complete!");
+						unregisterReceiver(this);
+						
+					}};
+				registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+			}
+		});
+		
+		//OnTouch
+		webview1.setOnTouchListener(new View.OnTouchListener(){
+				@Override
+				public boolean onTouch(View v, MotionEvent event){
+						int ev = event.getAction();
+						switch (ev) {
+								case MotionEvent.ACTION_DOWN:
+								
+								 
+								
+								break;
+								case MotionEvent.ACTION_UP:
+								
+								 
+								
+								break;
+						} return true;
+				}
+		});
+		
+		webview1.setWebViewClient(new WebViewClient() {
+			@Override
+			public void onPageStarted(WebView _param1, String _param2, Bitmap _param3) {
+				final String _url = _param2;
+				
+				super.onPageStarted(_param1, _param2, _param3);
+			}
+			
+			@Override
+			public void onPageFinished(WebView _param1, String _param2) {
+				final String _url = _param2;
+				
+				super.onPageFinished(_param1, _param2);
+			}
+		});
+	}
+	
+	private void initializeLogic() {
+		webview1.loadUrl("https://youtu.be/EvU6T4ZWi-c");
+		webview2.loadUrl("https://youtu.be/ZfasgdvjWzU");
+		webview3.loadUrl("https://youtu.be/frz4qXzeLVQ");
+		webview4.loadUrl("https://youtu.be/0FcVu-BlGQg");
+	}
+	
+	@Override
+	protected void onActivityResult(int _requestCode, int _resultCode, Intent _data) {
+		
+		super.onActivityResult(_requestCode, _resultCode, _data);
+		
+		switch (_requestCode) {
+			
+			default:
+			break;
+		}
+	}
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input){
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels(){
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels(){
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+	
+}
